d99c4d81e2192a7ff54ce75995592c98495172a6
Lukas Carvajal <lukascarvajal@gmail.com>
2023-07-29 19:28:17 +0200

    Initial Commit

---
+++ UbahnStationWegbeschreibung/UbahnStationWegbeschreibungApp.swift
@@ -0,0 +1,17 @@
+//
+//  UbahnStationWegbeschreibungApp.swift
+//  UbahnStationWegbeschreibung
+//
+//  Created by Lukas Carvajal on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct UbahnStationWegbeschreibungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
