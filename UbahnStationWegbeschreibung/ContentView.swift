3e4fc5cdebde09a803586aef71d6aa5c683901e8
Lukas Carvajal <lukascarvajal@gmail.com>
2023-07-29 21:07:07 +0200

    Initial commit

--- UbahnStationWegbeschreibung/ContentView.swift
+++ UbahnStationWegbeschreibung/ContentView.swift
@@ -8,14 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var locationViewModel = CoreLocationViewModel()
+    
+    var body: some View {
+        switch locationViewModel.authorizationStatus {
+        case .notDetermined:
+            AnyView(RequestLocationView())
+                .environmentObject(locationViewModel)
+        case .restricted:
+            ErrorView(errorText: "Location use is restricted.")
+        case .denied:
+            ErrorView(errorText: "The app does not have location permissions. Please enable them in settings.")
+        case .authorizedAlways, .authorizedWhenInUse:
+            TrackingView()
+                .environmentObject(locationViewModel)
+        default:
+            Text("Unexpected status")
+        }
+    }
+}
+
+struct RequestLocationView: View {
+    @EnvironmentObject var locationViewModel: CoreLocationViewModel
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image(systemName: "location.circle")
+                .resizable()
+                .frame(width: 100, height: 100, alignment: .center)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            Button(action: {
+                locationViewModel.requestPermission()
+            }, label: {
+                Label("Allow tracking", systemImage: "location")
+            })
+            .padding(10)
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            Text("We need your permission to track you.")
+                .foregroundColor(.gray)
+                .font(.caption)
+        }
+    }
+}
+
+struct ErrorView: View {
+    var errorText: String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "xmark.octagon")
+                    .resizable()
+                .frame(width: 100, height: 100, alignment: .center)
+            Text(errorText)
         }
         .padding()
+        .foregroundColor(.white)
+        .background(Color.red)
+    }
+}
+
+struct TrackingView: View {
+    @EnvironmentObject var locationViewModel: CoreLocationViewModel
+    
+    var body: some View {
+        VStack {
+            Button(action: {
+                locationViewModel.locateBeacon()
+            }, label: {
+                Label("Locate Beacon", systemImage: "location")
+            })
+        }
     }
 }
 
---
+++ UbahnStationWegbeschreibung/CoreLocationViewModel.swift
@@ -0,0 +1,72 @@
+//
+//  CoreLocationViewModel.swift
+//  UbahnStationWegbeschreibung
+//
+//  Created by Lukas Carvajal on 7/29/23.
+//
+
+import Foundation
+import CoreLocation
+
+class CoreLocationViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    @Published var authorizationStatus: CLAuthorizationStatus
+    
+    private let locationManager: CLLocationManager
+    
+    override init() {
+        locationManager = CLLocationManager()
+        authorizationStatus = locationManager.authorizationStatus
+        
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+    }
+    
+    func requestPermission() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func requestAlwaysPermission() {
+        locationManager.requestAlwaysAuthorization()
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+        
+        if authorizationStatus == .authorizedWhenInUse {
+            requestAlwaysPermission()
+        }
+    }
+    
+    func locateBeacon() {
+        let beaconID = "com.example.myBeaconRegion"
+        let region = CLBeaconRegion(uuid: UUID(uuidString: "F9DF84FC-1145-4D0B-9AC7-F2FAD5EFF690")!, identifier: beaconID)
+        
+        if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+            locationManager.startMonitoring(for: region)
+        }else {
+           print("CLLocation Monitoring is unavailable")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager,
+                didEnterRegion region: CLRegion) {
+        print("Region entered")
+        if region is CLBeaconRegion {
+            // Start ranging only if the devices supports this service.
+            if CLLocationManager.isRangingAvailable() {
+                manager.startRangingBeacons(in: region as! CLBeaconRegion)
+
+
+                // Store the beacon so that ranging can be stopped on demand.
+//                beaconsToRange.append(region as! CLBeaconRegion)
+                print("Beacon found")
+                print(region)
+            }
+            else {
+                print("nein!")
+            }
+        }
+    }
+}
